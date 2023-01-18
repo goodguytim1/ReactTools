cc347d8ab2b6596b94e45dc58571a352bc6eca1c
goodguytim1 <timobrien56@knights.ucf.edu>
2023-01-18 12:51:22 -0500

    fork

--- src/App.js
+++ src/App.js
@@ -1,47 +1,74 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import logo from "./logo.svg";
 import "./App.css";
-import Loader from "./components/Loader";
+import * as XLSX from "xlsx";
 
 function App() {
-  const [users, setUsers] = useState([{ name: "sam" }]);
-  const [isBusy, setIsBusy] = useState(false);
+  const [items, setItems] = useState([]);
 
-  useEffect(() => {
-    loadData();
-  }, []);
+  const readExcel = (file) => {
+    const promise = new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsArrayBuffer(file);
 
-  const loadData = () => {
-    setIsBusy(true);
-    const fakeData = [
-      {
-        name: "mike",
-      },
-      {
-        name: "samurai",
-      },
-      {
-        name: "jack",
-      },
-      {
-        name: "ryu",
-      },
-    ];
-    setTimeout(() => {
-      setUsers([...users, ...fakeData]);
-      setIsBusy(false);
-    }, 3000);
+      fileReader.onload = (e) => {
+        const bufferArray = e.target.result;
+
+        const wb = XLSX.read(bufferArray, { type: "buffer" });
+
+        const wsname = wb.SheetNames[0];
+
+        const ws = wb.Sheets[wsname];
+
+        const data = XLSX.utils.sheet_to_json(ws);
+
+        resolve(data);
+      };
+
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+
+    promise.then((d) => {
+      console.log(d)
+      d = d.filter(ds => ds.UserStatus === "Active")
+      console.log(d)
+      setItems(d);
+    });
   };
 
   return (
-    <div className="App m-4">
-      <Loader isBusy={isBusy}></Loader>
-      <ul className="list-group">
-        {users.map((d) => (
-          <li className="list-group-item">{d.name}</li>
-        ))}
-      </ul>
+    <div>
+      <input
+        type="file"
+        onChange={(e) => {
+          const file = e.target.files[0];
+          readExcel(file);
+        }}
+      />
+
+      <table class="table container">
+        <thead>
+          <tr>
+            <th scope="col">First Name</th>
+            <th scope="col">Last Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Sales To Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((d) => (
+            <tr key={d['UserID']}>
+              <th>{d['Agent first name']}</th>
+              <td>{d['Agent last name']}</td>
+              <td>{d['Agent email']}</td>
+              <td>{d['Agent total sales to date']}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
-
 export default App;
